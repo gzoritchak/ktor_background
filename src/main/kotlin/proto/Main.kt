fc5940ff6c603338132333b2751a3a346edf253d
gzoritchak <g.zoritchak@gmail.com>
2019-04-13 21:00:02 +0200

    Shutdown.

--- src/main/kotlin/proto/Main.kt
+++ src/main/kotlin/proto/Main.kt
@@ -12,38 +12,41 @@ import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
+import java.util.concurrent.TimeUnit
 
 val LOG = LoggerFactory.getLogger("proto")
 
+var stopping = false
 
-fun main() = runBlocking {
+suspend fun main() = coroutineScope {
+
     launch(Dispatchers.Default) {
         LOG.info("launch background")
         backgroundRequests()
     }
 
-    launch(Dispatchers.Default) {
-        LOG.info("launch ktor")
-        startServer()
-    }
-    LOG.info("Should never be logged")
+    val server = startServer()
+
+    Runtime.getRuntime().addShutdownHook(object : Thread() {
+        override fun run() {
+            stopping = true
+            server.stop(2L, 2L, TimeUnit.SECONDS)
+            println("Shutdown hook ran!")
+        }
+    })
 }
 
-fun startServer() {
-    val server = embeddedServer(Netty, port = 8080) {
+fun startServer() = embeddedServer(Netty, port = 8080) {
         routing {
             get("/") {
                 call.respondText("Hello world ${results.size}")
             }
         }
-    }
-    server.start(wait = true)
-    LOG.info("ktor started")
-}
+    }.start()
 
 val httpClient = HttpClient(Apache)
 var results = listOf<Int>()
@@ -52,7 +55,7 @@ suspend fun backgroundRequests() {
     LOG.info("backgroundRequests")
     var loop = 0
     val loader = Loader()
-    while (true) {
+    while (!stopping) {
         LOG.info("loop ${loop++}")
         try {
             results = results + loader.loadIntegers()
@@ -61,6 +64,7 @@ suspend fun backgroundRequests() {
         }
         delay(500)
     }
+    LOG.info("stopping background requests")
 }
 
 class Loader
@@ -75,3 +79,4 @@ private suspend fun request(getUrl: String): List<Int> {
         else -> error("Bad request?")
     }
 }
+
